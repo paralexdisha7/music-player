cb432c55ee5cace2ba44bd0dc3885ab2bc19a848
paralexdisha7 <paraledisha20@gmail.com>
2024-11-13 23:38:33 +0530

    fa1

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors({ origin: 'http://13.201.124.53:5173' }));
+app.use(cors({ origin: 'http://15.207.249.179:5173' }));
 
 // Configure AWS SDK
 const s3Client = new S3Client({
